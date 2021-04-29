067affd29cc3ce2ffe9ed7934fc8f142acd13ede
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2021-04-29 19:29:36 +0000

    Created CLI class

--- lib/gvl_restaurants.rb
+++ lib/gvl_restaurants.rb
@@ -1,4 +1,6 @@
-require "gvl_restaurants/version"
+require_relative "gvl_restaurants/version"
+require_relative "gvl_restaurants/CLI"
+
 
 module GvlRestaurants
   class Error < StandardError; end
---
+++ lib/gvl_restaurants/CLI.rb
@@ -0,0 +1,8 @@
+class GvlRestaurants::CLI
+  def intro
+    puts "Welcome to Greenville!"
+    puts "In which romantic location would you like to dine?"
+    puts " "
+    puts "Don't know which location you would like? Let us choose a random location for you!"
+  end
+end
